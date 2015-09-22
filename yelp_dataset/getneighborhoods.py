b752d8dfa91510c4e154e691901d4aec6792fe60
astrophilip <astrophilip@gmail.com>
2015-09-22 14:19:40 -0400

    added neighborhood bus count

--- yelp_dataset/getneighborhoods.py
+++ yelp_dataset/getneighborhoods.py
@@ -8,17 +8,28 @@ import ipdb
 import random
 import time
 
-neighborhoods = \
+neighborhoods = { 'BOS' :\
 ['Allston/Brighton' , 'Arlington_Center' , 'Arlington_Heights','Back_Bay', \
 'Beacon_Hill','Brookline_Village','Central_Square','Charlestown','Chinatown',\
 'Coolidge_Corner','Davis_Square','Dorchester','Downtown','Dudley_Square',\
 'East_Arlington','East_Boston','East_Cambridge','Egleston_Square','Fenway',\
 'Fields_Corner','Financial_District', \
-'Harvard_Square','Huron_Village' , 'Hyde_Park','Inman_Square','Jamaica_Plain', \
+'Harvard_Square','Huron_Village' , 'Hyde_Park','Inman_Square','Jamaica_Plain',\
 'Kendall_Square/MIT','Leather_District','Mattapan','Mattapan_Square',\
 'Mission_Hill','North_Cambridge','North_End','Porter_Square','Roslindale', \
 'Roslindale_Village','South_Boston','South_End','Teele_Square', \
-'Uphams_Corner','Waterfront','West_Roxbury','West_Roxbury_Center','Winthrop']
+'Uphams_Corner','Waterfront','West_Roxbury','West_Roxbury_Center','Winthrop']\
+}
+
+
+counts = { 'BOS':\
+    [1778,  660, 325, 4509, 820, 418, 1049, 1048, 874, 661, 457, 2652, \
+    4753, 221, 571, 1669, 1123, 120, 398, 105, 5597, 1408, 159, 1028, 222, \
+    1188, 1083, 427, 828, 128, 151, 741, 1142, 476, 842, 182, 4518, 1979, \
+    94, 117, 3133, 1043, 312, 521]
+    }
+
+city_prefix = {'BOS' : "p:MA:Boston::"}
 
 
 def readparse(s,inputurl):
@@ -92,22 +103,26 @@ def parse_ratings(html):
 
 
 
-def yelp_by_neighborhood(neighborhood,max_search=1000,sleep=True):
+def yelp_by_neighborhood(city,neighborhood,max_search=1000,sleep=True):
     # scrapes yelp results page for business information in map script
-    tst = "http://www.yelp.com/search?&l=p:MA:Boston::" + neighborhood + "&start=0"
+    tst = "http://www.yelp.com/search?&l="+city_prefix[city]\
+            + neighborhood + "&start=0"
     s = requests.Session()
     html = readparse(s, tst)
-    pgntn = html.find_all("span",{"class":"pagination-results-window"})[0].text
 
+    pgntn = html.find_all("span",{"class":"pagination-results-window"})[0].text
     m = re.search(r'of\s(.+)',pgntn)
     N = int(m.group(1))
+
     print N
     maxN = min(N,max_search)
+
     biz_dict = {}
     for start_page in range(0,maxN,10):
         # get all script tags
         print start_page
-        tst = "http://www.yelp.com/search?&l=p:MA:Boston::" + neighborhood + "&start=" + str(start_page)
+        tst = "http://www.yelp.com/search?&l=" +city_prefix[city] \
+                + neighborhood + "&start=" + str(start_page)
         html = readparse(s, tst)
 
         latlon_dict = parse_latlon(html)
@@ -131,16 +146,31 @@ def yelp_by_neighborhood(neighborhood,max_search=1000,sleep=True):
             time.sleep(random.uniform(1,4))
     return N, biz_dict
 
-def record_neighborhood(n_id,biz_dict):
-    filename = 'neighborhoods/BOS_'+str(n_id)+'.json'
+def record_neighborhood(city, nid,biz_dict):
+    filename = 'neighborhoods/'+ city + '_' +  str(nid)+'.json'
     with open(filename,'w') as f:
         json.dump(biz_dict,f)
 
-def parse_all_neighborhoods(neighborhoods,start_num,sleep= True):
+def parse_all_neighborhoods(city, neighborhoods,start_num,sleep= True):
     counts = []
     for neighborhood,nid in zip(neighborhoods[start_num:],range(start_num,len(neighborhoods))):
         print neighborhood
-        N, biz_dict = yelp_by_neighborhood(neighborhood,sleep=sleep)
+        N, biz_dict = yelp_by_neighborhood(city, neighborhood,sleep=sleep)
         record_neighborhood(nid, biz_dict)
         counts.append(N)
     print counts
+
+def parse_num_bus_per_neighborhood(city, neighborhoods):
+    s = requests.Session()
+    nids = []
+    counts = []
+    for nid,neighborhood in enumerate(neighborhoods):
+        tst = "http://www.yelp.com/search?&l=" +city_prefix[city] + \
+                neighborhood+ "&start=0"
+        html = readparse(s,tst)
+        pgntn = html.find_all("span",{"class":"pagination-results-window"})[0].text
+        m = re.search(r'of\s(.+)',pgntn)
+        N = int(m.group(1))
+        counts.append(N)
+        nids.append(nid)
+    return nids,counts
